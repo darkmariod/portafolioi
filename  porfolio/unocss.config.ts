ec760ab329fcd136658651b4abf3b747386f1ad5
Mario <109123293+darkmariod@users.noreply.github.com>
2025-01-29 20:46:54 -0500

    updated services

---  porfolio/unocss.config.ts
+++  porfolio/unocss.config.ts
@@ -1,26 +1,22 @@
+// unocss.config.ts
 import { defineConfig, presetUno } from 'unocss'
 
 export default defineConfig({
-  presets: [
-    presetUno({
-      dark: 'class'
-    })
-  ],
+  presets: [presetUno()],
   theme: {
     colors: {
-      primary: '#3B82F6',
-      secondary: '#6366F1',
+      primary: '#3B5E76',
       gray: {
-        50: '#f9fafb',
-        100: '#f3f4f6',
-        200: '#e5e7eb',
-        300: '#d1d5db',
-        400: '#9ca3af',
-        500: '#6b7280',
-        600: '#4b5563',
-        700: '#374151',
-        800: '#1f2937',
-        900: '#111827'
+        50: '#f8fafc',
+        100: '#f1f5f9',
+        200: '#e2e8f0',
+        300: '#cbd5e1',
+        400: '#94a3b8',
+        500: '#64748b',
+        600: '#475569',
+        700: '#334155',
+        800: '#1e293b',
+        900: '#0f172a'
       }
     }
   }
